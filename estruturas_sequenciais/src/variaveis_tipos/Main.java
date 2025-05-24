ff9a9d6abf1da2a768032654d503079aadd061d8
Plotegher <campinhos.ads@gmail.com>
2025-05-24 16:16:02 -0300

    Variáveis e tipos básicos em Java

---
+++ estruturas_sequenciais/src/variaveis_tipos/Main.java
@@ -0,0 +1,23 @@
+package variaveis_tipos;
+
+// Capítulo 02 / aula 03 - Variáveis e tipos básicos em Java
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      System.out.println("\nTipos Primitivos | Classes Wrapper");
+      System.out.println("---------------- | ---------------");
+      System.out.println(" byte            | Byte");
+      System.out.println(" short           | Short");
+      System.out.println(" int             | Integer");
+      System.out.println(" long            | Long");
+      System.out.println(" float           | Float");
+      System.out.println(" double          | Double");
+      System.out.println(" char            | Character");
+      System.out.println(" boolean         | Boolean");
+
+
+
+   }
+}
