d79efd34750b6b783c34b6b2a17af27f608c4d90
Plotegher <campinhos.ads@gmail.com>
2025-05-24 15:52:32 -0300

    Expressões aritméticas em Java

---
+++ estruturas_sequenciais/src/expressoes_aritmeticas/Main.java
@@ -0,0 +1,30 @@
+package expressoes_aritmeticas;
+
+// Capítulo 02 / aula 02 - Expressões aritméticas em Java
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      System.out.print("2 * 6 / 3 = ");
+      System.out.println(2 * 6 / 3);
+
+      System.out.print("3 + 2 * 4 = ");
+      System.out.println(3 + 2 * 4);
+
+      System.out.print("(3 + 2) * 4 = ");
+      System.out.println((3 + 2) * 4);
+
+      System.out.print("60 / (3 + 2) * 4 = ");
+      System.out.println(60 / (3 + 2) * 4);
+
+      System.out.print("60 / ((3 + 2) * 4) = ");
+      System.out.println(60 / ((3 + 2) * 4));
+
+      System.out.print("14 % 3 = ");
+      System.out.println(14 % 3);
+
+      System.out.print("19 % 5 = ");
+      System.out.println(19 % 5);
+   }
+}
