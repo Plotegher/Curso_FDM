334825244fcd2a44367b63ff301859680c1eea54
Plotegher <campinhos.ads@gmail.com>
2025-05-27 08:32:28 -0300

    Expressões comparativas em Java

---
+++ estruturas_condicionais/src/expressoes_comparativas/Main.java
@@ -0,0 +1,20 @@
+package expressoes_comparativas;
+
+// Módulo 04 / Capítulo 03 / aula 02 - Expressões comparativas em Java
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      System.out.println("OPERADORES COMPARATIVOS:");
+      System.out.println("------------------------");
+      System.out.println("Símbolo | Operador");
+      System.out.println(" >      | Maior");
+      System.out.println(" <      | Menor");
+      System.out.println(" >=     | Maior ou igual");
+      System.out.println(" <=     | Menor ou igual");
+      System.out.println(" ==     | Igual");
+      System.out.println(" !=     | Difrente");
+      System.out.println("------------------------");
+   }
+}
